79490c1f96dfcccdd671b7992c6bae6c1cae3ed9
ash1285 <320944803+ash1285@users.noreply.github.com>
2026-08-28 16:25:05 +0100

    Add REST API for Usuario: serializer, viewset and routes

---
+++ usuarios/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import UsuarioViewSet
+
+router = DefaultRouter()
+router.register('usuarios', UsuarioViewSet)
+
+urlpatterns = [path('', include(router.urls))]
