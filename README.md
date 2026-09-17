affaf18882f3586f0002ea21e4acbe5050b5a043
ash1285 <320944803+ash1285@users.noreply.github.com>
2026-09-17 11:40:56 +0100

    Switch database to PostgreSQL and add custom user manager

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # api-medica
 Medical records API with role-based access for patients and doctors. Built with Django, DRF, PostgreSQL and Docker
+
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 
 
 class Papel(models.TextChoices):
@@ -11,6 +12,18 @@ class Especialidade(models.TextChoices):
     CARDIOLOGISTA = 'cardiologista', 'Cardiologista'
     NEUROLOGISTA = 'neurologista', 'Neurologista'
 
+class UsuarioManager(BaseUserManager):
+    def create_user(self, nif, password=None, **extra_fields):
+        usuario = self.model(nif=nif, **extra_fields)
+        usuario.set_password(password)
+        usuario.save()
+        return usuario
+
+    def create_superuser(self, nif, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(nif, password, **extra_fields)
+
 class Usuario(AbstractUser):
     nif = models.CharField(max_length=9, unique=True)
     USERNAME_FIELD = "nif"
@@ -18,3 +31,4 @@ class Usuario(AbstractUser):
     especialidade = models.CharField(max_length=30, blank=True, choices=Especialidade.choices) 
     username = None
     REQUIRED_FIELDS =[]
+    objects = UsuarioManager()
